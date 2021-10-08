20262cbe7b1001d1860667787943f251729c6a26
AlessioToniolo <atoniolo76@gmail.com>
2021-10-08 13:02:14 +0000

    Create ButtonOpacity file

---
+++ src/Components/ButtonOpacity.js
@@ -0,0 +1,7 @@
+// Importing [blank] from React in order to [blank]
+import {} from "react";
+
+export default function ButtonOpacity(props) {
+    // Initalizing variables for useState
+    const [touched, setTouched] = React.useState(false);
+}

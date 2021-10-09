2f53b3deeab050e16bce420aa4505888f27dfbc4
AlessioToniolo <atoniolo76@gmail.com>
2021-10-09 03:03:16 +0000

    Remove ButtonOpacity component for TouchableOpacity

--- src/Components/ButtonOpacity.js
+++
@@ -1,7 +0,0 @@
-// Importing [blank] from React in order to [blank]
-import {} from "react";
-
-export default function ButtonOpacity(props) {
-    // Initalizing variables for useState
-    const [touched, setTouched] = React.useState(false);
-}
